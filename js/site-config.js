b5189e9c371aa5bfb0d3608bcc78eaaf4482b46d
Thomas <Thomaso.halbach@gmail.com>
2026-04-24 20:59:54 +0200

    Add files via upload

---
+++ js/site-config.js
@@ -0,0 +1,8 @@
+/**
+ * Öffentliche Basis-URL deines Portfolios (Ordner thomas-portfolio), ohne Slash am Ende.
+ * Wird für alle [data-portfolio-link] im Hub verwendet (Zum Portfolio / Footer).
+ *
+ * Beispiel: "https://thomasportfolio.pages.dev"
+ * Leer lassen = Links bleiben "#"; Tooltip erklärt’s (siehe portfolio-link.js).
+ */
+window.PORTFOLIO_SITE_URL = "";
